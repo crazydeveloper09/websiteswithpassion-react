fbe6c83ddceec2af8005d678ebadec4ef86a621a
crazydeveloper09 <maciejkuta6@gmail.com>
2022-03-14 14:53:11 +0100

    Delete server folder from client branch

--- server/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { loginUser, logoutUser } from '../controllers';
-
-const router = express.Router();
-
-router.post("/login", loginUser);
-router.post("/logout", logoutUser);
-
-export default router
