bc2b65f63ea5546e23add7c8de418c4a683e3d5b
crazydeveloper09 <maciejkuta6@gmail.com>
2022-03-16 10:08:53 +0100

    Better responsiveness and styling

--- src/components/common/Loading/Loading.tsx
+++ src/components/common/Loading/Loading.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import './Loading.scss';
-
-
+import "./Loading.scss";
 
 const Loading: React.FC = () => {
   return (
-    <svg className="spinner" viewBox="0 0 50 50">
-      <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
-    </svg>
-  )
-  
-}
+    <div className="loader-container">
+      <div className="loader-container__loader" />
+    </div>
+  );
+};
 
 export default Loading;
