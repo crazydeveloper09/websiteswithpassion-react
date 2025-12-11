178983f9cb545f831271080bdb1eeb3032138104
crazydeveloper09 <39824136+crazydeveloper09@users.noreply.github.com>
2025-12-11 19:42:19 +0100

    Change button styles from yellow to green

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,13 +11,13 @@ const Header: React.FC = () => {
                 <h1>{t('Witaj na moim portfolio!')}</h1>
                 <p>Maciej Kuta, UI Engineer</p>
                 <div className="header__links">
-                    <a href="#whatIdo" className="button button-grey">
+                    <a href="#whatIdo" className="button button-yellow">
                         {t('O mnie')}
                     </a>
-                    <Link to={'/projects'} className="button button-grey margin">
+                    <Link to={'/projects'} className="button button-yellow margin">
                         {t('Projekty')}
                     </Link>
-                    <Link to={'/website-orders/description'} className="button button-grey margin">
+                    <Link to={'/website-orders/description'} className="button button-yellow margin">
                         {t('Oferta')}
                     </Link>
                 </div>
